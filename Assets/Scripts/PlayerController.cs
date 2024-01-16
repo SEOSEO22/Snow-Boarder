890218b23be4eb86e7a8a6aa192125f98f49096a
Cha Seoyeon <seoyeon0833@gmail.com>
2024-01-16 15:17:55 +0900

    방향키를 사용해 속도 조절을 할 수 있도록 수정
    FindObjectOfType을 이용해 SurfaceEffector2D에 접근한 후, Up 방향키를
    누를 시 속도가 빨리지도록 조정

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float torqueSize = 1f;
+    [SerializeField] private float boostSpeed = 30f;
+    [SerializeField] private float baseSpeed = 20f;
     Rigidbody2D rigid;
+    SurfaceEffector2D surface;
 
     void Start()
     {
-        rigid = GetComponent<Rigidbody2D>();   
+        rigid = GetComponent<Rigidbody2D>();
+        surface = FindObjectOfType<SurfaceEffector2D>();
     }
 
     void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    private void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            surface.speed = boostSpeed;
+        else surface.speed = baseSpeed;
+    }
+
+    private void RotatePlayer()
     {
         // 플레이어 회전 설정
         if (Input.GetKey(KeyCode.LeftArrow))
